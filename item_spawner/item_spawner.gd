92b23bde162c551613017b702b2cdc784aa02a25
Kamil Lewan <carmel4a97@gmail.com>
2017-12-03 18:02:47 +0100

    Refactor item_spawner

--- item_spawner/item_spawner.gd
+++ item_spawner/item_spawner.gd
@@ -2,28 +2,33 @@ extends Sprite
 
 export var item_type = "Item"
 export var item_ammount = 2
-var player = null
+
+onready var player = system.player
+var _is_there_a_player = false
+const PLAYER_AREA_NAME = "player_area"
 
 func _ready():
+	
 	set_process(true)
 	
 func _process(delta):
+	
 	if player == null:
 		return
-	if not player.item and Input.is_action_pressed("ui_accept"):
-		prints("spawning item")
+	if _is_there_a_player and\
+			not player.item and\
+			Input.is_action_pressed("ui_accept"):
 		var n = load("res://item_spawner/%s.tscn" % item_type).instance()
 		n.set_name("item")
 		player.item = n
 		player.add_child(n)
 
 func _on_pick_area_area_enter( area ):
-	if area.get_name() == "player_area":
-		print("player enters pick area")
-		player = area.get_parent()
-		#co kurwa?
-
+	
+	if area.get_name() == PLAYER_AREA_NAME:
+		_is_there_a_player = true
 
 func _on_pick_area_area_exit( area ):
-	if area.get_name() == "player_area":
-		player = null
+	
+	if area.get_name() == PLAYER_AREA_NAME:
+		_is_there_a_player = false
