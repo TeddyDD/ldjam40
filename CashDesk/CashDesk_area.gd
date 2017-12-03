fd356b0c31e3507ea3c21a33a3df6c7df668f53b
Kamil Lewan <carmel4a97@gmail.com>
2017-12-03 23:16:36 +0100

    Visual check desk work.

--- CashDesk/CashDesk_area.gd
+++ CashDesk/CashDesk_area.gd
@@ -1,15 +1,29 @@
 extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var inventory
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	
+	inventory = get_node("inventory")
 
 
 func _on_CashDesk_area_area_enter( area ):
+	
 	if area.get_name() == "player_area":
 		pass
+	if area.get_name() == "pushing_area":
+		get_node("Timer").start()
+
+func _check():
+	if !system.trolley.inventory.is_empty():
+		system.trolley.inventory.move_item_to(0, inventory)
+
+func _on_Timer_timeout():
+	
+	_check()
+
+func _on_CashDesk_area_area_exit( area ):
+	if area.get_name() == "player_area":
+		pass
+	if area.get_name() == "pushing_area":
+		get_node("Timer").stop()
