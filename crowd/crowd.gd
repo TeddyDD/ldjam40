20e75ebbcdd5bb4e7d2d140fbeed7f979dfc7e74
Kamil Lewan <carmel4a97@gmail.com>
2017-12-04 10:28:57 +0100

    Working movement of crowd.

--- crowd/crowd.gd
+++ crowd/crowd.gd
@@ -6,15 +6,16 @@ var b = 0
 func set_state(i):
 	if i == 0:
 		get_node("crowd_a").hide()
-		a = 2
+		a = 0
 	if i == 1:
 		get_node("crowd_a").show()
-		b = 2
+		get_node("crowd_a/Area2D1").set_monitorable(true)
+		get_node("crowd_a/Area2D1").set_enable_monitoring(true)
+		b = 0
 		a = 1
 	if i == 2:
-		get_node("crowd_a").show()
-		b = 2
-		a = 2
+		b = 1
+		a = 1
 		set_process(false)
 		get_node("Timer").stop()
 	change()
@@ -23,12 +24,22 @@ func change():
 	set_region_rect(Rect2(Vector2(a*64, 0), Vector2(64, 32)))
 	get_node("crowd_a").set_region_rect(Rect2(Vector2(b*64, 0), Vector2(64, 32)))
 
+func new_():
+	
+	var n = load("res://crowd/crowd.tscn").instance()
+	var nn = get_parent().add_child(n)
+	n.set_global_pos(get_global_pos()-Vector2(0, 64))
+
 func _ready():
 	
 	get_node("Timer").start()
-
-
+	set_state(state)
 
 func _on_Timer_timeout():
 	state += 1
 	set_state(state)
+	if state == 2:
+		new_()
+
+func _on_Area2D_area_enter( area ):
+	pass # replace with function body
--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -8,18 +8,13 @@ var path = null
 var close_to = null
 var inventory
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	inventory = get_node("inventory")
 	set_process(true)
 	
 func _process(delta):
 	get_node("debug").set_text(get_node("FSM2D").getStateID())
-	pass
-	
+
 func _draw():
 	if path != null:
 		for p in path:
@@ -36,13 +31,10 @@ class Sorter:
 
 func _on_hit_area_area_enter( area ):
 	if area.get_parent().is_in_group("spawner"):
-#	if area.get_name() == "pick_area":
 		prints("NEAR SPAWNER")
-#		if not area.get_parent().inventory.is_empty():
 		close_to = area.get_parent()
 	if area.is_in_group("item"):
 		if area.get_parent().flies == true:
 			hit_by = area.get_parent()
 		elif area.get_parent().is_active == true:
 			close_to = area.get_parent()
-
